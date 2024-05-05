6c224cad50fe2892f7156c84cdf3bcc5ef849037
hdfredl <dat.leef@gmail.com>
2024-05-05 20:08:06 +0200

    yes finally klar..

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const taskName = document.querySelector("#name-box");
 const taskPriority = document.querySelector("#priority-box");
 const taskDescription = document.querySelector("#desc-box");
 const addNewTask = document.querySelector(".add-task");
+const completeSelectedTask = document.querySelector(".complete-task");
+//const editSelectedTask = document.querySelector(".edit-task");
 
 const modal = document.getElementById("myModal");
 const span = document.querySelector(".close");
@@ -16,8 +18,14 @@ const span = document.querySelector(".close");
 // EVENTLISTENERS ---
 createTask.addEventListener("click", openNewModal);
 addNewTask.addEventListener("click", createNewTask);
+// editSelectedTask.addEventListener("click", editThisTask);
 span.addEventListener("click", closeModal);
 window.addEventListener("click", handleOutsideClick);
+
+const editSelectedTask = document.querySelector(".edit-task");
+if (editSelectedTask) {
+  editSelectedTask.addEventListener("click", editThisTask);
+}
 
 // FUNCTIONS
 function displayTasks(todos) {
@@ -31,13 +39,19 @@ function displayTasks(todos) {
       <p class="description-task">${todo.description}</p>
       <div>
         <button class="complete-task">Complete</button>
-        <button class="delete-task" onclick="deleteTask(${todo.id})">Delete</button>
-        <button class="edit-task">Edit</button>
+        <button class="delete-task" onclick="deleteTask('${todo.id}')">Delete</button>
+        <button class="edit-task" onclick="editThisTask('${todo.id}')">Edit</button>
       </div>
     </div>
   `;
     containerToDisplay.innerHTML += html;
   }); // TODO: GÖR DESCRIPTION HIDDEN
+
+  // Gå igenom alla .complete-task och poppa completeThisTask - grön / vit textfärg
+  const completeButtons = document.querySelectorAll(".complete-task");
+  completeButtons.forEach((button) => {
+    button.addEventListener("click", () => completeThisTask(button));
+  });
 }
 
 function createNewTask() {
@@ -63,11 +77,59 @@ function deleteTask(taskId) {
   const deleteSelectedTask = {
     method: "DELETE",
   };
+
+  fetch(`http://localhost:3000/todos/${taskId}`, deleteSelectedTask).then(
+    () => {
+      const taskElement = document.getElementById(`task-${taskId}`);
+      if (taskElement) {
+        taskElement.remove();
+      }
+    }
+  );
+}
+
+function editThisTask(taskId) {
+  // öppna modal
+  openNewModal(true);
+
+  const saveButton = document.querySelector(".save-button");
+  saveButton.addEventListener("click", function () {
+    const updatedTask = {
+      task: taskName.value,
+      priority: taskPriority.value,
+      description: taskDescription.value,
+    };
+
+    const putTask = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedTask),
+    };
+
+    fetch(`http://localhost:3000/todos/${taskId}`, putTask)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Task updaterad:", data);
+        closeModal();
+      });
+  });
+}
+
+function completeThisTask(button) {
+  const currentColor = button.style.color;
+  if (currentColor === "green") {
+    button.style.color = "";
+  } else {
+    button.style.color = "green";
+  }
 }
 
 // MODAL -
-function openNewModal() {
+function openNewModal(editMode = false) {
   modal.style.display = "block";
+
+  const saveButton = document.querySelector(".save-button");
+  saveButton.disabled = !editMode;
 }
 
 function closeModal() {
